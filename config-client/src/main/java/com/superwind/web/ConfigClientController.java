7fc7faaa58c5e00b7d0966d4fa496d8b6cfc1976
jiangxj <jiangxj@wenwen-tech.com>
2017-07-01 10:24:40 +0800

    增加config-client

---
+++ config-client/src/main/java/com/superwind/web/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.superwind.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigClientController {
+
+    @Autowired
+    Environment env;
+
+    @Value("${person.name}")
+    String name;
+
+    @PostMapping("/config/qryUser")
+    public String qryUser() {
+        System.out.println("person.name="+name+" in the config-repo");
+        return name;
+    }
+}
