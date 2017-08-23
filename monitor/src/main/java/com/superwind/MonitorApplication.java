abe4d19b587b5520c536108581899a646634aa7f
jiangxj <jiangxj@wenwen-tech.com>
2017-08-23 16:29:47 +0800

    增加spring-boot-admin监控server

--- monitor/src/main/java/com/superwind/MonitorApplication.java
+++ monitor/src/main/java/com/superwind/MonitorApplication.java
@@ -1,15 +1,15 @@
 package com.superwind;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 @EnableEurekaClient
-@EnableHystrixDashboard
-@EnableTurbine
 public class MonitorApplication {
 
 	public static void main(String[] args) {
