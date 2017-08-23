c607cbc04a7d0cad5019f17ed9b6b20b14543f2f
jiangxj <jiangxj@wenwen-tech.com>
2017-08-23 16:33:40 +0800

    增加zipkin client

--- service-provider/src/main/java/com/superwind/service/ServiceProviderService.java
+++ service-provider/src/main/java/com/superwind/service/ServiceProviderService.java
@@ -1,5 +1,6 @@
 package com.superwind.service;
 
+import com.superwind.conf.ProviderProperties;
 import com.superwind.pojo.QryUserRsp;
 import com.superwind.pojo.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,15 @@ import java.util.List;
 public class ServiceProviderService {
     @Autowired
     Environment env;
-
-    String name;
+    @Autowired
+    private ProviderProperties providerProperties;
 
     public int addUser(UserInfo userInfo) {
-        System.out.println("addUser id="+userInfo.getId()+"  name="+name+"  sex="+userInfo.getSex());
+        System.out.println("addUser id="+userInfo.getId()+"  name="+userInfo.getName()+"  sex="+userInfo.getSex());
+        System.out.println(providerProperties.getTestA());
+        System.out.println(providerProperties.getTestB());
+        System.out.println(providerProperties.getOps().getAccessLog());
+        System.out.println(providerProperties.getOps().getErrorLog());
         return 1;
     }
 
