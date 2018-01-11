4108a577140fda5eb4f20b3e5f987a10d50ab282
jiangxj <jiangxj@wenwen-tech.com>
2018-01-11 15:15:19 +0800

    更新版本

--- hystrix-dashboard/src/main/java/com/superwind/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/superwind/HystrixDashboardApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
-@EnableTurbine
 public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
